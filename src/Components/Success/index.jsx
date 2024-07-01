c3147068c704521a3911e569c3459c1dc02d6558
RushikeshKokate22 <rushikesh.kokate@tradebrains.in>
2024-07-01 15:57:28 +0530

    Success page is created. after user login it will redirect to this page

---
+++ src/Components/Success/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./success.css"
+
+const Success = () => {
+  return (
+    <div className='success'> 
+    <p>Your response has been submitted successfully. Please login to continue.</p>
+    </div>
+  )
+}
+
+export default Success
